6f1c96ea843090813072b94cf106dd348846b059
dzneladze1lile <dzneladzeliles@gmail.com>
2026-09-04 00:31:43 +0400

    seventy-second commit

---
+++ level71/base-apparel-coming-soon-master/script.js
@@ -0,0 +1,23 @@
+const sumbit = document.querySelector(".submit");
+const error = document.querySelector(".error");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const label = document.querySelector("label");
+
+form.addEventListener("submit", validate);
+sumbit.addEcentListener("click", validate);
+
+function validate(e) {
+    e.preventDefault()
+
+    const inputValue = input.value.trim();
+    console.log(inputValue);
+
+    if (inputValue === "") {
+        error.style.display = "block";
+    } else if (!isEmail(inputValue)) {
+        error.style.display = "block"
+    } else {
+        error.style.display = "none"
+    }
+}
