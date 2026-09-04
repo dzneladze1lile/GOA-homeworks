e5fa3861b5bd5b3c9273b6dfb1874d461298d4c4
dzneladze1lile <dzneladzeliles@gmail.com>
2026-09-04 15:54:56 +0400

    eighty-ninth commit

---
+++ level88/classwork/classwork.js
@@ -0,0 +1,24 @@
+for (let i = 4; i < 10; i++) {
+    console.log(i);
+}
+
+for (let i = 5; i < 15; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i < 21; i++) {
+    console.log(i);
+}
+
+
+let i = 0;
+while (i < 10) {
+   console.log(i);
+   i++
+}
+
+let x = 15;
+do {
+    console.log(x);
+    x--
+} while (x > 0)
---
+++ level88/homework/homework.js
@@ -0,0 +1,38 @@
+for (let i = 0; i <= 38; i++) {
+    console.log(i);
+}
+
+for (let i = 30; i < 40; i++) {
+    console.log(i);
+}
+
+for (let i = 12; i < 120; i++) {
+    console.log(i);
+}
+
+
+for (let i = 0; i < 18; i++) {
+    console.log("Lile");
+}
+
+for (let i = 10; i < 100; i+= 3) {
+    console.log(i);
+}
+
+for (let i = 0; i <=30; i++) {
+    console.log(i + " banana")
+}
+
+
+let i = 0;
+while (i < 10) {
+    console.log("Lile");
+    i++
+}
+
+let x = 0;
+while (x < 24) {
+    console.log(x);
+    x++
+}
+
